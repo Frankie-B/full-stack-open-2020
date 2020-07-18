00e5da108176791f3b70ec0d176bd6c866535f96
Francis Bukenya <frankie.bukenya@gmail.com>
2020-07-18 10:58:45 +0200

    Fix linting

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,8 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => {
+  return (
+    <h1>{props.name}</h1>
+  )
+
+};
+
+const Content = (props) => {
+  return (
+    <div>
+    <p>{props.part} {props.exercise}</p>
+    <p>{props.part} {props.exercise}</p>
+    <p>{props.part} {props.exercise}</p>
+    </div>
+  
+  )
+};
+
+const Totals = (props) => { };
+
 const App = () => {
-  const course = 'Half Stack application development'
+    const course = 'Half Stack application development'
+
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
