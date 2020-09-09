daa396fbdf5141ead062efbba02529f9ddbb08aa
Frankie Bukenya <frankie.bukenya@gmail.com>
2020-09-09 12:15:27 +0200

    Add anecdotes WIP

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,17 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+function getRandom(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+};
+
+const Anecdote = ({text, votes=0}) => {
+  return (
+    <div>
+      <p>{text}</p>
+      <p>has {votes} votes</p>
+    </div>
+  )
+}
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = (props) => {
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState({});
+  const [mostVotes, setMostVotes] = useState(0);
+
+  const vote = () => {
+    const copy = [...votes];
+    copy[selected] += 1;
+
+    if (copy[selected] > votes[mostVotes]) {
+      setMostVotes(selected);
+    };
+
+    setVotes(copy);
+
+  }
+
+  const nextAnecdote = () => {
+    setSelected(getRandom(props.anecdotes.length))
+  }
+
+  return (
+    <div>
+      <h2>Anecdote of the day</h2>
+
+      <Anecdote
+        text={props.anecdotes[selected]}
+        votes={votes[selected]}
+      />
+
+      <Button handleClick={vote} text={'Vote'} />
+   
+    </div>
+  );
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+];
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+);
