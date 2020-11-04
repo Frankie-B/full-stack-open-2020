5217c1c11aa0d4a0ec3344f09872c192fce59139
Frankie Bukenya <frankie.bukenya@gmail.com>
2020-11-04 12:18:13 +0100

    Add 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import axios from 'axios';
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
-  
+  const [persons, setPersons] = useState([]);
   const [showPersons, setShowPersons] = useState(persons);
   const [newFilter, setNewFilter] = useState('');
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+
+  useEffect(() => {
+    console.log('Effect');
+
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('We have data!!', response);
+        setPersons(response.data);
+      })
+  },[])
+
 
   const handleNewName = (event) => {
     setNewName(event.target.value);
@@ -57,4 +64,3 @@ function App() {
 }
 
 export default App;
-
