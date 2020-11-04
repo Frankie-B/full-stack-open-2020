b77b48ed66665d7f72a9a6d91adf6da8c47e0b05
Frankie Bukenya <frankie.bukenya@gmail.com>
2020-11-04 15:26:13 +0100

    Start Countries app

---
+++ part2/countries/src/App.js
@@ -0,0 +1,12 @@
+// import { useEffect, useState } from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hey there frankie!!</h1>
+    </div>
+  );
+}
+
+export default App;
